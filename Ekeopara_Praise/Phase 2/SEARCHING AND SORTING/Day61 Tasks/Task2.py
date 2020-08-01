6e4f15f28a686d1d66eeaa9f7902c870e694b0b5
Ekeopara-Praise <ekeoparapraise@gmail.com>
2020-08-01 23:25:41 +0100

    Day61 Task Solution

---
+++ Ekeopara_Praise/Phase 2/SEARCHING AND SORTING/Day61 Tasks/Task2.py
@@ -0,0 +1,21 @@
+'''2. Write a Python program for sequential search.
+Sequential Search : In computer science, linear search or sequential search is a method for finding a particular value in a list that 
+checks each element in sequence until the desired element is found or the list is exhausted. The list need not be ordered.
+Test Data :
+Sequential_Search([11,23,58,31,56,77,43,12,65,19],31) -> (True, 3) '''
+
+
+def Sequential_Search(dlist, item):
+
+    pos = 0
+    found = False
+    
+    while pos < len(dlist) and not found:
+        if dlist[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+    
+    return found, pos
+
+print(Sequential_Search([11,23,58,31,56,77,43,12,65,19],31))
---
+++ Ekeopara_Praise/Phase 2/SEARCHING AND SORTING/Day61 Tasks/Task3.py
@@ -0,0 +1,22 @@
+'''3. Write a Python program for binary search for an ordered list.
+Test Data :
+Ordered_binary_Search([0, 1, 3, 8, 14, 18, 19, 34, 52], 3) -> True
+Ordered_binary_Search([0, 1, 3, 8, 14, 18, 19, 34, 52], 17) -> False'''
+
+def binary_search(item_list,item):
+	first = 0
+	last = len(item_list)-1
+	found = False
+	while( first<=last and not found):
+		mid = (first + last)//2
+		if item_list[mid] == item :
+			found = True
+		else:
+			if item < item_list[mid]:
+				last = mid - 1
+			else:
+				first = mid + 1	
+	return found
+	
+print(binary_search([1,2,3,5,8], 6))
+print(binary_search([1,2,3,5,8], 5))
