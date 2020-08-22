3077feac1717d35f267bcc8e8d014b12a6899e2b
Ekeopara-Praise <ekeoparapraise@gmail.com>
2020-08-22 21:04:30 +0100

    Day74 Task Solution

---
+++ Ekeopara_Praise/Phase 2/RECURSION/Day74 Tasks/Task1.py
@@ -0,0 +1,6 @@
+'''1. Write a Python program to calculate the sum of a list of numbers.'''
+
+def sum_list(List):
+    result = sum(List)
+    return result 
+print(sum_list([1, 2,  3,  4, 5, 6, 7, 8, 9]))
